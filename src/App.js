d3925a2b64035b2e89aa47ba8896a48ca5f5fe5d
Junior <juninhoc4p@gmail.com>
2024-10-11 18:48:19 -0300

    Aula sobre useEfect

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-function App(){
-
-  const [ reverse, setReverse ] = useState(false);
-  const [ counter, setCounter ] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
-
-  function handleClick(){
-
-    setReverse((prevReverse) => !prevReverse);
-    
-  }
-
-  function handleIncrement(){
-    setCounter((prevCounter) => prevCounter + 1);
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-
-        <h1>Contador: {counter}</h1>
-        
-        <button type='button' onClick={handleClick}>
-          Reverse
-        </button>
-
-        <button type='button' onClick={handleIncrement}>
-          Incrementar
-        </button>
-      </header>
-    </div>
-  );
-
+function eventFn() {
+    console.log('H1 clicado');
 }
 
-export default App; 
+function App() {
+    const [counter, setCounter] = useState(0);
+    const [counter2, setCounter2] = useState(0);
+
+    // Component didUpdate = executa toda vez que o componente atualiza
+    /*useEffect(() => {
+        console.log('Componente didUpdate');
+    }); */
+
+    // Component didUpdate = executa uma vez
+    useEffect(() => {
+        document.querySelector('h1')?.addEventListener('click', eventFn);
+
+        // ComponentWillUmount - limpeza
+        return () => {
+            document.querySelector('h1')?.removeEventListener('click', eventFn);
+        };
+    }, []);
+
+    // Component dependecia executa toda vez que a dependencia mudar
+    useEffect(() => {
+        console.log('Counter foi atualizado', counter);
+    }, [counter, counter2]);
+
+    return (
+        <div className="App">
+            <p>Teste 1</p>
+            <h1>Contador {counter}</h1>
+            <h1>Contador 2 {counter2}</h1>
+            <button type="button" onClick={() => setCounter(counter + 1)}>
+                +
+            </button>
+            <button type="button" onClick={() => setCounter2(counter2 + 1)}>
+                +
+            </button>
+        </div>
+    );
+}
+
+export default App;
